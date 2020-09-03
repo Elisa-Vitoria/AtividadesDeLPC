c3fbe0de11d7d9d6ddc33395f5c3a15984597180
Elisa-Vitoria <68713600+Elisa-Vitoria@users.noreply.github.com>
2020-09-03 17:17:49 -0300

    Atividades 11 e 12

---
+++ atv_11_e_12.py
@@ -0,0 +1,88 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "atv.11 e 12.py",
+      "provenance": [],
+      "authorship_tag": "ABX9TyOFhaZ7x4SPpyp11xI4ArxL",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Elisa-Vitoria/AtividadesDeLPC/blob/master/atv_11_e_12.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "r6duxIqwmxyp",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 34
+        },
+        "outputId": "d467bdab-38d4-4175-98c6-1bf241b7e9d2"
+      },
+      "source": [
+        "# ETE PORTO DIGITAL\n",
+        "# LPC - Introdução a Python\n",
+        "# Prof. Cloves Rocha\n",
+        "# Estudante: Elisa Vitória\n",
+        "# Turma: 1°B\n",
+        "\n",
+        "#Imprimir seu nome completo usando o comando print()\n",
+        "#atv.11\n",
+        "\n",
+        "print('Elisa Vitória Alves de Lima')"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Elisa Vitória Alves de Lima\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "42YsL-bNnCPK",
+        "colab_type": "code",
+        "colab": {}
+      },
+      "source": [
+        "# ETE PORTO DIGITAL\n",
+        "# LPC - Introdução a Python\n",
+        "# Prof. Cloves Rocha\n",
+        "# Estudante: Elisa Vitória\n",
+        "# Turma: 1°B\n",
+        "\n",
+        "#Codificar no Colab ou lápis e papel uma contagem regressiva (exemplo do foguete espacial NASA).\n",
+        "#atv.12\n",
+        "\n",
+        "from time import sleep\n",
+        "for cont in range(10, -1, -1):\n",
+        "    print(cont)\n",
+        "    sleep(1)\n",
+        "print('Decolar!')"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
